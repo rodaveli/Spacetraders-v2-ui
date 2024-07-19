ee4662224829283f551fd5fc57091fb1670fdcd8
rodaveli <rodrigoabreu56@gmail.com>
2024-07-19 17:45:35 -0500

    ayyyy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import React from 'react';
 import './App.css';
 import { getAgentData, getContracts, getServerStatus, getShips } from './utils/api';
 import Dashboard from './components/Dashboard';
